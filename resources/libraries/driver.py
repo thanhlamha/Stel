cd596d7f4a850fd46f3dc3ceee04245f868b83c9
thanhlamha <lamha.work2011@gmail.com>
2024-06-18 23:46:30 +0700

    k

---
+++ resources/libraries/driver.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+
+opts = FirefoxOptions()
+opts.add_argument('--headless')
+browser = webdriver.Firefox(options=opts)
+
+browser.get('https://example.com')
